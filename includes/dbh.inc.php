561509340a2a2e7b5388ec111205c32202460063
cmd_fklr <lab_damjanovicn@protonmail.com>
2025-10-08 00:59:09 +0200

    switched db connection from mysqli to PDO

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,6 +2,9 @@
 // define('ROOT_PATH', dirname(__DIR__) . '/');
 // $env = parse_ini_file(ROOT_PATH.'.env');
 
-$conn = mysqli_connect(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQLDATABASE"), getenv("MYSQLPORT"));
+$dsn = "mysql:" . getenv("MYSQLUSER") . ":" . getenv("MYSQLPASSWORD") . "@" . getenv("MYSQLHOST") . ":" . getenv("MYSQLPORT") . "/railway";
 
-if ($conn -> connect_error) die(`Connection failed: ` . $conn -> connect_error);
+// $conn = new PDO(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQLDATABASE"), getenv("MYSQLPORT"));
+$conn = new PDO($dsn);
+
+if (!$conn) die(`Connection failed: ` . $conn);
