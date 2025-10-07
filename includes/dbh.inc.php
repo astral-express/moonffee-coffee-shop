0f86dcfa62bf6897d51a66ff043472625c0aef12
cmd_fklr <lab_damjanovicn@protonmail.com>
2025-10-08 00:25:15 +0200

    db update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,6 +2,6 @@
 // define('ROOT_PATH', dirname(__DIR__) . '/');
 // $env = parse_ini_file(ROOT_PATH.'.env');
 
-$conn = mysqli_connect(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQL_DATABASE"), 3306);
+$conn = mysqli_connect(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQLDATABASE"), getenv("MYSQLPORT"));
 
 if ($conn -> connect_error) die(`Connection failed: ` . $conn -> connect_error);
