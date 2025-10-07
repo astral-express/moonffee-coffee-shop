387db012daa09f2c1cb2102049305f1f08ed5d91
cmd_fklr <lab_damjanovicn@protonmail.com>
2025-10-07 23:45:25 +0200

    port added

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,6 +2,6 @@
 // define('ROOT_PATH', dirname(__DIR__) . '/');
 // $env = parse_ini_file(ROOT_PATH.'.env');
 
-$conn = new mysqli(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQL_DATABASE"));
+$conn = new mysqli(getenv("MYSQLHOST"), getenv("MYSQLUSER"), getenv("MYSQLPASSWORD"), getenv("MYSQL_DATABASE"), 3306);
 
 if ($conn -> connect_error) die(`Connection failed: ` . $conn -> connect_error);
