bd667c4aeca4182bc59daf0ea6ac5a0fadf9be72
cmd_fklr <lab_damjanovicn@protonmail.com>
2025-10-07 22:57:07 +0200

    db creds updated

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,16 @@
 <?php
-define('ROOT_PATH', dirname(__DIR__) . '/');
-$env = parse_ini_file(ROOT_PATH.'.env');
 
-// $env = parse_ini_file(ROOT_PATH.'db_test.ini');
+// db test
+// define('ROOT_PATH', dirname(__DIR__) . '/');
+// $env = parse_ini_file(ROOT_PATH.'.env');
+// $conn = new mysqli($env["MYSQLHOST"], $env["MYSQLUSER"], $env["MYSQLPASSWORD"], $env["MYSQL_DATABASE"]);
 
-$conn = new mysqli($env["MYSQLHOST"], $env["MYSQLUSER"], $env["MYSQLPASSWORD"], $env["MYSQL_DATABASE"]);
+$database = getenv("${{Moonffee-DB.MYSQL_DATABASE}}");
+$hostname = getenv("${{Moonffee-DB.MYSQLHOST}}");
+$password = getenv("${{Moonffee-DB.MYSQLPASSWORD}}");
+$username = getenv("${{Moonffee-DB.MYSQLUSER}}");
+$port = 3306;
+
+$conn = new mysqli($hostname, $username, $password, $database, $port);
 
 if ($conn -> connect_error) die(`Connection failed: ` . $conn -> connect_error);
